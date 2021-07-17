710daf3c2ae585e4406634f75611f6e204cc52fc
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2021-07-18 00:37:22 +0530

    add streamlit to req.txt
    * streamlit reports Cython error 
    need to fix that

---
+++ upi random forests/app.py
@@ -0,0 +1,81 @@
+from flask import Flask,request
+import pandas as pd
+import numpy as np
+import pickle
+import flasgger
+from flasgger import Swagger
+
+
+
+# Whenever this line is get, the flask server will start from name == main
+app=Flask(__name__)
+Swagger(app)
+pickle_in=open('classifier.pkl','rb')
+classifier=pickle.load(pickle_in)
+
+
+# By default a GET request is given
+@app.route('/')
+def welcome():
+    return "Welcome All"
+
+
+@app.route('/predict',methods=['GET'])
+def predict_note_authentication():
+    """Let's Authenticate the Banks Note
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: variance
+        in: query
+        type: number
+        required: true
+      - name: skewness
+        in: query
+        type: number
+        required: true
+      - name: curtosis
+        in: query
+        type: number
+        required: true
+      - name: entropy
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+
+    """
+    variance=request.args.get('variance')
+    skewness=request.args.get('skewness')
+    curtosis=request.args.get('curtosis')
+    entropy=request.args.get('entropy')
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    return str(prediction)
+
+
+@app.route('/predict_files',methods=['POST'])
+def predict_note_file():
+    """Let's Authenticate the Banks Note
+        This is using docstrings for specifications.
+        ---
+        parameters:
+          - name: file
+            in: formData
+            type: file
+            required: true
+
+        responses:
+            200:
+                description: The output values
+
+        """
+    df_test=pd.read_csv(request.files.get('file'))
+    prediction=classifier.predict(df_test)
+
+    return  str(list(prediction))
+
+
+if __name__=='__main__':
+    app.run(debug=True)
